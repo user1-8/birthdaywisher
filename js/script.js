055c06441b82bd9722e38a3454069ffb74042d69
adam18092005 <adam18092005@gmail.com>
2021-02-07 17:31:25 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,112 @@
+const btn = document.getElementById('btn_go');
+const datepicker = document.querySelector('.datepicker');
+const dateselector = document.getElementById('dateselector');
+
+// below function is for closing the result
+function closeResult(){
+  document.querySelector('.result_wrapper').setAttribute('style', 'opacity:0; pointer-events: none;');
+}
+
+
+
+
+
+
+// below function copies the result link
+function copyLink(r){
+  /* Get the text field */
+  var link = document.querySelector('.link');
+
+  /* Select the text field */
+  link.select();
+  link.setSelectionRange(0, 9999999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  r.setAttribute('style', 'background:#05c46b;');
+  r.innerHTML = ' Copied <i class="far fa-copy"></i>';
+  setTimeout(function(){
+    r.setAttribute('style', 'background:#4e16be;');
+    r.innerHTML = ' Copy <i class="far fa-copy"></i>';
+  }, 2000);
+}
+
+
+
+// below is onsubmit btn click
+const letsgo = () => {
+  
+  setTimeout(function(){
+    //document.querySelector('.page-transition').classList.add('animating-now');
+  }, 900);
+  
+
+  // add url to the tag
+  document.querySelector('.link').innerHTML = "main.html?"+"m="+document.querySelector('.msg').value+"&"+"zpw=zpdskfdsffjklejzikwenkejwknoq"+"&"+"d=2021-"+document.getElementById('monthselector').value+"-"+document.getElementById('dateselector').value+"&"+"rn="+document.querySelector('.receivername').value+"&"+"cn="+document.querySelector('.creatorname').value;
+  
+  //set href of view btn to the link's value with the &amp; converted to &
+  document.querySelector('.view').setAttribute('href', document.querySelector('.link').innerHTML.replace(/&amp;/g, '&'));
+
+  // set href of whatsapp anchor tag
+  document.querySelector('.whatsapp_share').setAttribute('href', 'adf');
+  
+  // show result
+  setTimeout(function(){
+    document.querySelector('.result_wrapper').setAttribute('style', 'opacity:1; pointer-events: auto;');
+  }, 300);
+
+}
+
+
+const enable_btn = () => {
+
+  let namerules=(/^[A-Za-z]+$/);
+  let allnameright = document.querySelector('.receivername').value.length>1 && document.querySelector('.receivername').value.match(namerules) && document.querySelector('.creatorname').value.length>1 && document.querySelector('.creatorname').value.match(namerules);
+
+  if(document.getElementById('dateselector').value!="" && document.getElementById('monthselector').value!="" && allnameright && document.querySelector('.msg').value.length>1){
+    btn.removeAttribute('disabled', '');
+  }else{
+    btn.setAttribute('disabled', '');
+  }
+}
+
+setTimeout(enable_btn, 500);
+
+
+
+
+// below function is to be used by another function
+function rmAndDates(n, action="rm"){
+  if(action == 'rm'){
+    for(var i=n; i<=31; i++){
+      dateselector.children[i].style.display = 'none';
+      // below removes eg. if one chose 31 first and then chose feb then... so that he cannot enter 31 in feb
+      if(document.getElementById('dateselector').value == i){
+        document.getElementById('dateselector').value = "";
+      }
+    }
+  }
+
+  if(action == 'add'){
+    // below loop for changing display to normal for the others
+    for(var j=(--n); j>=1; j--){
+      dateselector.children[j].style.display = null;
+    }
+  }
+}
+
+editDates = (g) =>{
+
+  rmAndDates(32, 'add');
+
+  if(g.value == 2) rmAndDates(30)
+  if(g.value == 4) rmAndDates(31)
+  if(g.value == 6) rmAndDates(31)
+  if(g.value == 9) rmAndDates(31)
+  if(g.value == 11) rmAndDates(31)
+  
+}
+
+
+
---
+++ js/script1.js
@@ -0,0 +1,112 @@
+let now, dateArr, dest, dif, raw, sec, min, hour, days, birthday;
+
+
+
+
+// below function accesses the url variables
+const getUrlParams = (url) => {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
+
+if(getUrlParams(window.location.href).m!=undefined && getUrlParams(window.location.href).zpw=="zpdskfdsffjklejzikwenkejwknoq" && getUrlParams(window.location.href).d!=undefined && getUrlParams(window.location.href).rn!=undefined && getUrlParams(window.location.href).cn!=undefined){
+  birthday = getUrlParams(window.location.href).d;
+  document.getElementById('name').innerHTML = getUrlParams(window.location.href).rn;
+  document.querySelector('.byWhom .cname').innerHTML = getUrlParams(window.location.href).cn;
+  document.querySelector('.msg_area .indication .cname').innerHTML = getUrlParams(window.location.href).cn;
+  document.querySelector('.msg_area .msg').innerHTML = getUrlParams(window.location.href).m;
+}else{
+  window.location.href="index.html";
+}
+
+
+
+
+
+
+
+// all about timer starts here
+
+function assignNowAndDest(){
+  now = new Date(); //now time
+
+    
+  // taking value of date & time from input and entering that into the dest var
+
+  if(birthday!=undefined){
+    // dateArr = "2021-02-1".split('-');
+    dateArr = birthday.split('-');
+  }
+  
+  // conversion from string to int
+  dateArr[0] = parseInt(dateArr[0], 10);
+  dateArr[1] = parseInt(dateArr[1], 10);
+  dateArr[2] = parseInt(dateArr[2], 10);
+
+  dest = new Date(dateArr[0], dateArr[1]-1, dateArr[2], 0, 0); // -1 in month is becoz of getting months value 1-12 and js accepts 0-11 months
+
+
+  // assignNowAndDest(); // execute function on page load
+
+
+
+  dif = dest.getTime() - now.getTime();
+
+  raw = Math.trunc(dif/1000);
+
+  sec = raw%60;
+  min = Math.trunc((raw/60)%60);
+  hour = Math.trunc((raw/(60*60))%24);
+  days = Math.trunc(raw/(60*60*24));
+
+
+  // console.log(`${days} days ${hour} hours ${min} minutes ${sec} seconds`);
+}
+assignNowAndDest();
+
+// now assigning values into GUI
+
+const daysNum = document.getElementById('daysNum');
+const hourNum = document.getElementById('hourNum');
+const minNum = document.getElementById('minNum');
+const secNum = document.getElementById('secNum');
+
+function GUIassign(){
+  daysNum.innerHTML = days;
+  hourNum.innerHTML = hour;
+  minNum.innerHTML = min;
+  secNum.innerHTML = sec;
+}
+
+GUIassign(); // executing this function on page load
+
+setInterval(function(){
+  assignNowAndDest();
+  GUIassign();
+}, 1000);
+
+// all about timer ends here
+
+
+
+
+if(sessionStorage.getItem('dfs') == null){
+  setTimeout(function(){
+
+    document.querySelector('.page-transition').classList.add('animating-now');
+    
+    // below localstorage sets the item so that it could be accessed by another page
+    sessionStorage.setItem('dfs', 'kjkewjjjs');
+
+  }, 500);
+}else{
+  document.querySelector('.page-transition').classList.remove('page-transition');
+}
