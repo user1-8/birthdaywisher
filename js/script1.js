5b95a995813c5f483df8996f1b9c636d92e61fff
adam18092005 <adam18092005@gmail.com>
2021-02-14 14:54:50 +0530

    commited

--- js/script1.js
+++ js/script1.js
@@ -9,7 +9,7 @@ const getUrlParams = (url) => {
 	var parser = document.createElement('a');
 	parser.href = url;
 	var query = parser.search.substring(1);
-	var vars = query.split('&amp;');
+	var vars = query.split('&adm;');
 	for (var i = 0; i < vars.length; i++) {
 		var pair = vars[i].split('=');
 		params[pair[0]] = decodeURIComponent(pair[1]);
